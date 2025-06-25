bbd0745c9a37a69828179a3fb39e76df3c3fd96a
geanprn <geanpranadaniswara@gmail.com>
2025-06-25 12:35:33 +0800

    tambah fungsi addFirst dan addLast

--- C030324018.cpp
+++ C030324018.cpp
@@ -17,6 +17,21 @@ class KontakList {
     Kontak* head = nullptr, *tail = nullptr;
 public:
     ~KontakList() { while (head) { Kontak* t = head; head = head->next; delete t; } }
+
+    void addFirst(const string& n, const string& no, const string& e) {Add commentMore actions
+        Kontak* node = new Kontak(n, no, e);
+        if (!head) head = tail = node;
+        else { node->next = head; head = node; }
+        cout << "Kontak ditambah di awal.\n";
+    }
+    
+    void addLast(const string& n, const string& no, const string& e) {
+        Kontak* node = new Kontak(n, no, e);
+        if (!head) head = tail = node;
+        else { tail->next = node; tail = node; }
+        cout << "Kontak ditambah di akhir.\n";
+    }
+    
 };
 
 int main() {
